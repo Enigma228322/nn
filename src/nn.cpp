6ba9432c33590d0f26b6ab0de186ffa695c4cbf3
Enigma228322 <vildan.safin.99@gmail.com>
2021-11-23 22:37:46 +0500

    Start
    Signed-off-by: Enigma228322 <vildan.safin.99@gmail.com>

--- src/nn.cpp
+++ src/nn.cpp
@@ -2,11 +2,19 @@
 #include "nn.hpp"
 #include "spdlog/spdlog.h"
 #include "simdjson.h"
+#include <vector>
 
 using namespace simdjson;
 
 int main()
 {
-    spdlog::info("Init");
+    spdlog::info("Start shit");
+    ondemand::parser parser;
+    padded_string json = padded_string::load("config.json");
+    ondemand::document config = parser.iterate(json);
+
+    std::vector <int> layers;
+    for(auto now : config["layers"])
+        layers.push_back(now);
     return 0;
 }
